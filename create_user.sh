6f353f7d0b363e57bee4911b6bf2bff4bf223cc6
Choi Kunhan <ckh0618@gmail.com>
2025-01-12 05:32:28 +0900

    fix create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,15 +1,18 @@
+## Create Admin User 
 mongosh <<EOF
 use admin 
 db.createUser( {user : "admin", pwd : "admin", roles : ["root"]})
 exit
 EOF
 
+## Create App user 
 mongosh -u admin -p admin <<EOF
 use admin 
 db.createUser( {user : "app", pwd : "app", roles : ["root"]})
 exit
 EOF
 
+## Set inaccurate IP address
 mongosh -u admin -p admin <<EOF
 use admin 
 db.updateUser("app", {authenticationRestrictions: [{clientSource: ["127.0.0.1", "10.1.1.1"] }] })
@@ -17,6 +20,7 @@ exit
 EOF
 
 
+## Unset authenticationRestrictions
 mongosh -u admin -p admin <<EOF
 use admin 
 db.system.users.updateOne ( {_id :"admin.app" }, { '\$unset': { authenticationRestrictions: 1 }})
